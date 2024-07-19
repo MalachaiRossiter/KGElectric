237b74798b6aae849566c4aad192df8a56034ca8
Malachai Rossiter <malachai.rossiter@gmail.com>
2024-07-18 21:23:32 -0400

    Started Work on First Page

---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import {useEffect, useState } from 'react';
+
+const Home = (props) => {
+    
+
+    return (
+        <div className='container'>
+            <h1>Hello World
+            </h1>
+        </div>
+    )
+}
+export default Home;
