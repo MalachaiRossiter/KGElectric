5a7e58bb9630e9da72c182ddb9d1e028f2b6e286
Malachai Rossiter <malachai.rossiter@gmail.com>
2025-12-26 13:41:24 -0500

    Restructured how I like

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -2,12 +2,7 @@ const { Resend } = require("resend");
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const sendEmailService = async (
-    replyEmail,
-    subject,
-    body,
-    attachments = []
-) => {
+const sendEmailService = async (replyEmail, subject, body,attachments = []) => {
     const emailData = {
     from: process.env.EMAIL_FROM,
     to: process.env.EMAIL_RECEIVER,
